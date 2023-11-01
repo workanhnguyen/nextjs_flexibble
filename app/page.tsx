b4df80986d579bbaf426c08d2af5efd488fdca99
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-11-01 14:51:55 +0700

    feat: implement home page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </section>
+  );
+};
 
-export default HomePage
+export default HomePage;
